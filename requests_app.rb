19a74c4eeebf632de01b9426b990974313cb80db
Tom Dyer <tdyer1@gmail.com>
2013-10-07 08:57:05 -0400

    Added html examples and handler for POST requests

--- requests_app.rb
+++ requests_app.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'rack'
+require 'pry'
 
 class HandleRequest
   def get(env, path)
@@ -22,16 +23,31 @@ class HandleRequest
   end
 
   def post(env)
-    puts "post: path is #{ENV['PATH']}"
+    # http://rack.rubyforge.org/doc/classes/Rack/Request.html
+    request = Rack::Request.new(env)
+
+    # binding.pry
+    
+    body_hash = request.POST
+    reply_str = "<html><head></head><body><h2>Post Body is</h2><ul>"
+
+    puts "URL Parameters are:\n"
+
+    body_hash.each do |key, value|
+      puts "key = #{key}"
+      puts "value = #{value}"
+      reply_str << "<li>key is #{key}, value is #{value}</li>"
+    end
+    reply_str << "</ul></body><html>"
+    puts "Yep, it's form data" if request.form_data?
+
+    return [200, {"Content-Type" => "text/html"}, [reply_str]]
   end
 
   def call(env)
-    # show all the HTTP parameters
-    # env.each do |k,v|
-    #   puts "#{k} = #{v}"
-    # end
-
-    # reply =  [500, {"Content-Type" => "text/html"}, "<p>Error:</p>"][]
+    env.each do |k,v|
+      puts "#{k} = #{v}"
+    end
 
     if env['REQUEST_METHOD'] == 'GET'
       reply = self.get(env, env['PATH_INFO'])
