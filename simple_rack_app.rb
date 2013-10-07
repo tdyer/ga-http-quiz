fa24625d3df3e92de49bd1b172b16112c09f9c4a
Tom Dyer <tdyer1@gmail.com>
2013-10-07 12:02:36 -0400

    Add comments to simple_rack_app

--- simple_rack_app.rb
+++ simple_rack_app.rb
@@ -7,7 +7,9 @@ class Hello
       puts "#{k} = #{v}"
     end
     puts "PATH_INFO is #{env['PATH_INFO']}"
+    # Build the HTTP Response
+    # [status code, Response header, Response body]
     [200, {"Content-Type" => "text/html"}, "<p>hey there from simple class</p>"]
   end
 end
-Rack::Handler::Thin.run Hello.new, :Port => 1234
+Rack::Handler::Thin.run Hello.new, :Port => 8080
